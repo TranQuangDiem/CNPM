00cc5615cd4ed23493f3b978f15cc93463377e36
TranQuangDiem <67749229+TranQuangDiem@users.noreply.github.com>
2020-07-31 13:15:59 +0700

    Update SendMail.java
    -gửi mã xác thực mail đến người dùng

--- Demo/src/bookStore/forgotPass/SendMail.java
+++ Demo/src/bookStore/forgotPass/SendMail.java
@@ -8,5 +8,42 @@ import java.util.Date;
 import java.util.Properties;
 
 public class SendMail {
- 
+  public static int code;
+    public static void sendEmail(String host, String port,
+                                 String userName,  String password, String toAddress,String message) throws AddressException,
+            MessagingException {
+
+        // cấu hình SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.mime.charset","UTF-8");
+
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+
+        Session session = Session.getInstance(properties, auth);
+
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+        msg.setContent(msg, "UTF-8");
+
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = {new InternetAddress(toAddress)};
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        String sub = "Bạn muốn đổi mật khẩu ? ";
+        code = (int) Math.floor(((Math.random() * 899999) + 10000000));
+        msg.setSubject(sub);
+        msg.setSentDate(new Date());
+        msg.setText(message);
+
+        // sends the e-mail
+        Transport.send(msg);
+    }
 }
