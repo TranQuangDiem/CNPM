54cb50b38bc5bc851863e0051d155f90f39e1419
TranQuangDiem <67749229+TranQuangDiem@users.noreply.github.com>
2020-08-02 17:21:35 +0700

    Update doForgotPass.java

--- Demo/src/bookStore/forgotPass/doForgotPass.java
+++ Demo/src/bookStore/forgotPass/doForgotPass.java
@@ -16,11 +16,10 @@ import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
 import java.util.regex.Pattern;
 
 
-@WebServlet({"/BookStoredoForgotPass"})
+@WebServlet("/BookStoredoForgotPass")
 public class doForgotPass extends HttpServlet {
 
     private String host;
@@ -39,7 +38,7 @@ public class doForgotPass extends HttpServlet {
 
             request.setCharacterEncoding("utf-8");
             response.setCharacterEncoding("utf-8");
-            //4.lấy email người dùng nhập
+            //4.hệ thống lấy email người dùng nhập vào
             String email = request.getParameter("email") == null ? "" : request.getParameter("email");
             //tạo mã xác thực
             int code = (int) Math.floor(((Math.random() * 899999) + 10000000));
@@ -48,41 +47,46 @@ public class doForgotPass extends HttpServlet {
             String regexMail = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
             Pattern paMail = Pattern.compile(regexMail);
             boolean checkMail = false;
-            //kiểm tra email đã được đăng ký chưa
-            String sqlcheckExitsMail = "SELECT `user`.email FROM `user` WHERE email=?";
-            PreparedStatement psMail = DBConnect.getPreparedStatement(sqlcheckExitsMail);
-            psMail.setString(1, email);
-            ResultSet rsMail = psMail.executeQuery();
-            boolean existEmail = rsMail.next();
-            psMail.setString(1, email);
-            //người dùng nhập email đúng cú pháp không
-            if (paMail.matcher(email).matches() == false) {
-                errmail = "Email của bạn không hợp lê";
-            } else if (existEmail == false) {//email người dùng nhập không được đăng ký trong hệ thống
-                errmail = "Email của bạn không tồn tại trong hệ thống chúng tôi";
-            } else {
-                errmail = "Chúng tôi đã gửi mail cho bạn";
-                checkMail = true;
+            if (paMail.matcher(email).matches() == true) {
+                String sqlcheckExitsMail = "SELECT `user`.email FROM `user` WHERE email=?";
+                //5. hệ thống truy xuất đến database user
+                PreparedStatement psMail = DBConnect.getPreparedStatement(sqlcheckExitsMail);
+                psMail.setString(1, email);
+                //6. database user trả về kết quả
+                ResultSet rsMail = psMail.executeQuery();
+                boolean existEmail = rsMail.next();
+                psMail.setString(1, email);
+                //7.1 Hệ thống kiểm tra email không hợp lệ
+                //người dùng nhập email đúng cú pháp không
+                if (paMail.matcher(email).matches() == false) {
+                    errmail = "Email của bạn không hợp lê";
+                }else if (email.length()>50){
+                    errmail = "Email nhập vào quá dài";
+                }
+                else if (existEmail == false) {//email người dùng nhập không được đăng ký trong hệ thống
+                    errmail = "Email của bạn không tồn tại trong hệ thống chúng tôi";
+                }
+                else {
+                    checkMail = true;
+                }
+            }else {
+                errmail="email của bạn không hợp lệ";
             }
+            //7. Hệ thống kiểm tra email hợp lệ
             if (checkMail == true) {//Nếu email đã được đăng ký trong tài khoản
-                long millis = System.currentTimeMillis();
-                java.sql.Date date = new java.sql.Date(millis);
-                java.sql.Time time = new Time(millis);
-                //update lại thông tin email đó
-                String sqlUpdateforgot = "UPDATE test.user SET user.maxacthuc=?, user.ngaytao=? ,user.gio =? WHERE user.email=?";
+                //lưu lại mã xác thưc ,ngày, giờ của email xuống database
+                String sqlUpdateforgot = "UPDATE test.user SET user.maxacthuc=?, user.ngaytao=LOCALTIMESTAMP WHERE user.email=?";
                 PreparedStatement psUpdateforgot = DBConnect.getPreparedStatement(sqlUpdateforgot);
                 psUpdateforgot.setInt(1, code);
-                psUpdateforgot.setDate(2, date);
-                psUpdateforgot.setTime(3, time);
-                psUpdateforgot.setString(4,email);
+                psUpdateforgot.setString(2,email);
                 psUpdateforgot.executeUpdate();
-                String mess = "BookStore/changePass.jsp" + System.lineSeparator() + "Mã xác thực của bạn là    " + code;
-                //gửi mã xác thực về email người dùng nhập
+                String mess = "access link http://localhost:8080/Demo/BookStore/changePass.jsp" + System.lineSeparator() +"Your authentication code is: " + code + System.lineSeparator() + "Validation codes are valid for 30 minutes from now";
+                //8. Hệ thống gửi mã xác thực về email người dùng nhập
                 SendMail.sendEmail(host, port, user, pass, email, mess);
-          
+                //10.Hệ thống hiển thị trang changePass
                 response.sendRedirect("BookStore/changePass.jsp");
 
-            }else {//người dùng nhập sai email trả về lỗi
+            }else {//8.1. Hệ thống hiển thị lỗi ở trang forgotPassword.
                 request.setAttribute("email", email);
                 request.setAttribute("errmail", errmail);
                 request.getRequestDispatcher("BookStore/ForgotPassword.jsp").forward(request, response);
